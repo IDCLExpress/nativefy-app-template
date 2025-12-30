dfc0997617cf685f924a7dc06402431a7e7215de
IDCLExpress <Delcampoizzydj@aol.com>
2025-12-29 20:08:56 -0500

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+});
